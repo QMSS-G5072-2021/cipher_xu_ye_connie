d8c8d1f44945af51544914ca6799858759640476
connixu <y.connie.xu@gmail.com>
2021-11-10 00:06:14 -0500

    pytest added

--- tests/test_cipher_yx2625.py
+++ tests/test_cipher_yx2625.py
@@ -1,1 +1,7 @@
 from cipher_yx2625 import cipher_yx2625
+
+def test_cipher_encrypt(): 
+    example = ['Harry', 3]
+    expected = 'KduuB'
+    actual = cipher_yx2625(example[0],example[1])
+    assert actual == expected
