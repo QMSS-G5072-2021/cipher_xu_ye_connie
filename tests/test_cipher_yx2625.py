dfc1cba2a876778d71a84568bd147a19672a55a7
connixu <y.connie.xu@gmail.com>
2021-11-10 09:24:40 -0500

    Revert "Added readthedocs"
    This reverts commit 653c91404c69905440b59dcbc49ab7a7408662a5.

--- tests/test_cipher_yx2625.py
+++ tests/test_cipher_yx2625.py
@@ -1,1 +1,7 @@
-from cipher_yx2625 import cipher_yx2625
+from cipher_yx2625 import cipher_yx2625 as cph
+
+def test_cipher_encrypt(): 
+    example = ['Harry', 3]
+    expected = 'KduuB'
+    actual = cph.cipher(example[0],example[1])
+    assert actual == expected
